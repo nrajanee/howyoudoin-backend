66613511841a92a57f98861c3012ef2108dc3557
nrajanee <nikiniks14@gmail.com>
2018-12-04 19:57:06 -0500

    Added node_modules

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/views'));
-app.use(express.static(__dirname + '/scripts'));
+app.get('/',function(req,res){
+ if(!req.query.user){
+    return res.status(422).send({
+    errorType: 'RequestFormatError',
+    message: 'Must include the userName.',
+    });
+ }
+ if(!req.query.password){
+     return res.status(422).send({
+     errorType: 'RequestFormatError',
+     message: 'Must include the password.',
+     });
+  }
 
-app.get('/', function(req, res){
-   res.sendFile("views/Login.html",{root: __dirname});
-
-});
-
-app.get('/login',function(req,res){
-  res.send("Logged in");
-});
-
-app.get('/loginHandler.js',function(req,res){
-  res.sendFile("loginHandler.js",{root: __dirname});
-});
-app.get('/home',function(req,res){
-res.sendFile("views/Dashboard.html",{root: __dirname});
 });
 
 app.listen(3000);
