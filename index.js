3acb4efc20f1b08c4b15b1f0d68559e40acf7871
nrajanee <nikiniks14@gmail.com>
2018-12-05 23:02:02 -0500

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-var pg = require('pg');
+var cors = require('cors');
+app.use(cors); 
 const { Client } = require('pg');
 
 app.get('/login',function(req,res){
@@ -76,7 +77,7 @@ app.get('/login',function(req,res){
          message: 'Reached the login'
        });
 
- console.log("done");
+ //console.log("done");
 
 
 });
